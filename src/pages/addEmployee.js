dd664ac381d1b33e2ac20bf328e7d5279eb6b5fd
Durga Prasad <velkuru.durgaprasad@gmail.com>
2019-02-14 23:06:14 +0530

    Listing employee details from local json

--- src/pages/addEmployee.js
+++ src/pages/addEmployee.js
@@ -18,6 +18,30 @@ import Page from "components/Page";
 
 class addEmployee extends Component {
   state = {};
+  componentDidMount() {
+    fetch("./people.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          userList: data
+        });
+        console.log("data:", JSON.stringify(data));
+      });
+    // fetch("./people.json")
+    //   .then(response => {
+    //     console.log(response);
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     // Work with JSON data here
+    //     console.log(data);
+    //   })
+    //   .catch(err => {
+    //     // Do something for an error here
+    //     console.log("Error Reading data " + err);
+    //   });
+  }
   render() {
     return (
       <Page
--- src/pages/listEmployee.js
+++ src/pages/listEmployee.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import {
   Row,
   Col,
@@ -12,24 +13,88 @@ import {
   ButtonDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  DropdownItem,
+  Tooltip
 } from "reactstrap";
+import "../styles/style.css";
+
 class listEmployee extends Component {
-  state = {};
+  state = {
+    userList: [],
+    isLoaded: false
+  };
+
+  componentDidMount() {
+    fetch("./people.json")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          userList: data
+        });
+        console.log("data:", JSON.stringify(data));
+      });
+    // fetch("./people.json")
+    //   .then(response => {
+    //     console.log(response);
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     // Work with JSON data here
+    //     console.log(data);
+    //   })
+    //   .catch(err => {
+    //     // Do something for an error here
+    //     console.log("Error Reading data " + err);
+    //   });
+  }
+
+  // componentWillReceiveProps(nextprops) {
+  //   console.log("----------", nextprops);
+  // }
+
   handleclick = () => {
     this.props.history.push("/addEmployee");
     // window.location("");
   };
+
   render() {
+    var { isLoaded, items, userList } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
     const style = {
       marginLeft: "1609px"
     };
+    const tableStle = {
+      width: " 70%",
+      textAlign: "center",
+      marginLeft: "150px"
+    };
     return (
-      <ButtonGroup className="mr-3 mb-3">
-        <Button style={style} onClick={this.handleclick} color="primary">
-          Add
-        </Button>
-      </ButtonGroup>
+      <div>
+        <ButtonGroup className="mr-3 mb-3">
+          <Button style={style} onClick={this.handleclick} color="primary">
+            Add
+          </Button>
+        </ButtonGroup>
+        <table border="1" style={tableStle}>
+          <tbody>
+            <tr>
+              <th>Names</th>
+              <th>Rating</th>
+              <th>Description</th>
+            </tr>
+            {userList.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.rating}</td>
+                <td>{item.Description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
